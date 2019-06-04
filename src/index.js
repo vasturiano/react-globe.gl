a1e1d84320bb10d1c703d0d665595599d5ee8adb
vasco <vastur@gmail.com>
2019-06-04 01:23:52 -0700

    Add kapsule react wrapper

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import fromKapsule from 'react-kapsule';
+import GlobeKapsule from 'globe.gl';
+import GlobePropTypes from './globe-proptypes';
+
+const Globe = fromKapsule(
+  GlobeKapsule,
+  undefined,
+  [ // bind methods
+    'pauseAnimation',
+    'resumeAnimation',
+    'pointOfView',
+    'scene',
+    'camera',
+    'renderer',
+    'controls',
+    'getCoords',
+    'toGeoCoords',
+  ],
+  ['animateIn', 'rendererConfig']
+);
+
+Globe.displayName = 'Globe';
+Globe.propTypes = GlobePropTypes;
+
+export default Globe;
