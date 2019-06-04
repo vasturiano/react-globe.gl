3a3a25cf78e1b39c57108cb28f16ddee18d51e53
vasco <vastur@gmail.com>
2019-06-04 01:23:08 -0700

    Add repo scaffold

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
+
+export default {
+  external: ['react'],
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'Globe',
+      file: `dist/${name}.js`,
+      globals: { react: 'React' },
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    replace({ 'process.env.NODE_ENV': JSON.stringify('development') }), // To fool React in the browser
+    babel({ exclude: '**/node_modules/**' }),
+    resolve(),
+    commonJs()
+  ]
+};
